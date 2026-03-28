a563670abe6ecd380f76ca1c59acb45f9d98e94b
simofilail514-a11y <simofilail514@gmail.com>
2026-03-28 03:22:15 +0100

    Create pages/index.js with a basic React component

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Project</h1>
+      <p>Your website is live!</p>
+    </div>
+  );
+}
